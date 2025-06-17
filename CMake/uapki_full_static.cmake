34cbf00931683f5c8d84804f3bd4bca681cf8383
Volodymyr Sydorenko <v.m.sydorenko@gmail.com>
2025-06-17 17:11:40 +0300

    Виправлення помилок компіляції

--- CMake/uapki_full_static.cmake
+++ CMake/uapki_full_static.cmake
@@ -18,8 +18,27 @@ target_include_directories(asn1 PUBLIC
 )
 add_definitions(-DASN1_STATIC)
 
+# --- Збірка byte-array ---
+file(GLOB BYTE_ARRAY_SOURCES
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array-internal.c"
+)
+add_library(byte-array STATIC ${BYTE_ARRAY_SOURCES})
+target_include_directories(byte-array PUBLIC
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/include
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
+)
+if(WIN32)
+    target_link_libraries(byte-array PUBLIC bcrypt)
+endif()
+# Добавляем явные экспортируемые дефайны для byte-array
+target_compile_definitions(byte-array PUBLIC UAPKIC_STATIC UAPKIC_LIBRARY NOCRYPT _CRT_SECURE_NO_WARNINGS BA_STATIC)
+
 # --- Збірка ba-utils ---
-file(GLOB BA_UTILS_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/pkix/ba-utils.c")
+file(GLOB BA_UTILS_SOURCES 
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/pkix/ba-utils.c"
+)
 # Добавляем uapki-errors.c для реализации error_code_to_str
 list(APPEND BA_UTILS_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/pkix/uapki-errors.c")
 add_library(ba-utils STATIC ${BA_UTILS_SOURCES})
@@ -30,16 +49,56 @@ target_include_directories(ba-utils PUBLIC
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkif/src/asn1
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src
 )
-add_definitions(-DBA_STATIC)
+target_link_libraries(ba-utils PUBLIC byte-array stacktrace)
+target_compile_definitions(ba-utils PUBLIC UAPKIC_STATIC BA_STATIC)
+
+# --- Збірка stacktrace ---
+file(GLOB STACKTRACE_SOURCES 
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/stacktrace.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros/stacktrace.h"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/pthread-impl.c"
+)
+add_library(stacktrace STATIC ${STACKTRACE_SOURCES})
+target_include_directories(stacktrace PUBLIC
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/include
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
+)
+target_link_libraries(stacktrace PUBLIC byte-array)
+target_compile_definitions(stacktrace PUBLIC NOCRYPT _CRT_SECURE_NO_WARNINGS UAPKIC_STATIC)
+
+# --- Добавляем dirent-internal для поддержки директорий ---
+file(GLOB DIRENT_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapki/src/dirent-internal.c")
+add_library(dirent-internal STATIC ${DIRENT_SOURCES})
+target_include_directories(dirent-internal PUBLIC
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapki/src
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/pkix
+)
+target_link_libraries(dirent-internal PUBLIC stacktrace ba-utils)
 
 # --- Збірка uapkic ---
 file(GLOB UAPKIC_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/*.c")
+# Исключаем файлы ByteArray, так как они уже включены в отдельную библиотеку
+list(REMOVE_ITEM UAPKIC_SOURCES 
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array-internal.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/stacktrace.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/pthread-impl.c"
+)
 add_library(uapkic STATIC ${UAPKIC_SOURCES})
 target_include_directories(uapkic PUBLIC
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/include
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
 )
-target_compile_definitions(uapkic PRIVATE UAPKIC_STATIC UAPKIC_LIBRARY UAPKIC_SELF_TEST NOCRYPT _CRT_SECURE_NO_WARNINGS)
+target_compile_definitions(uapkic PUBLIC UAPKIC_STATIC UAPKIC_LIBRARY UAPKIC_SELF_TEST NOCRYPT _CRT_SECURE_NO_WARNINGS BA_STATIC)
+if(WIN32)
+    target_link_libraries(uapkic PUBLIC stacktrace byte-array bcrypt)
+else()
+    target_link_libraries(uapkic PUBLIC stacktrace byte-array)
+endif()
 
 # --- Збірка uapkif ---
 file(GLOB UAPKIF_ASN1_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkif/src/asn1/*.c")
@@ -57,12 +116,16 @@ target_compile_definitions(uapkif PRIVATE UAPKIF_STATIC UAPKIF_LIBRARY NOCRYPT _
 file(GLOB PARSON_SOURCES
     "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/json/parson.c"
     "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/json/parson-helper.cpp"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/json/parson-ba-utils.c"
     "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/json/strtod-no-locale.c"
 )
 add_library(parson STATIC ${PARSON_SOURCES})
 target_include_directories(parson PUBLIC
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/json
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/include
+    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
 )
+target_link_libraries(parson PUBLIC ba-utils byte-array)
 
 # --- Збірка cm-pkcs12 ---
 file(GLOB CM_PKCS12_SRC
@@ -95,7 +158,7 @@ target_include_directories(cm-pkcs12 PUBLIC
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/pkix
 )
-target_link_libraries(cm-pkcs12 PUBLIC uapkic uapkif parson)
+target_link_libraries(cm-pkcs12 PUBLIC uapkic uapkif parson stacktrace)
 target_compile_definitions(cm-pkcs12 PRIVATE CM_LIBRARY)
 if(WIN32)
     target_compile_definitions(cm-pkcs12 PRIVATE NOCRYPT _CRT_SECURE_NO_WARNINGS)
@@ -104,14 +167,7 @@ if(APPLE)
     target_link_libraries(cm-pkcs12 PRIVATE iconv)
 endif()
 
-# --- Добавляем dirent-internal для поддержки директорий ---
-file(GLOB DIRENT_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapki/src/dirent-internal.c")
-add_library(dirent-internal STATIC ${DIRENT_SOURCES})
-target_include_directories(dirent-internal PUBLIC
-    ${CMAKE_SOURCE_DIR}/extern/uapki/library/uapki/src
-    ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/macros
-)
-target_link_libraries(dirent-internal PUBLIC ba-utils)
+# --- dirent-internal уже объявлен ранее ---
 
 # --- Збірка uapki ---
 file(GLOB UAPKI_API_SOURCES "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapki/src/api/*.cpp")
@@ -138,30 +194,78 @@ target_include_directories(uapki PUBLIC
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/loaders
     ${CMAKE_SOURCE_DIR}/extern/uapki/library/common/curl/include
 )
-target_link_libraries(uapki PUBLIC uapkif uapkic asn1 ba-utils parson cm-pkcs12 dirent-internal)
-target_compile_definitions(uapki PRIVATE UAPKI_STATIC UAPKI_LIBRARY)
+target_link_libraries(uapki PUBLIC uapkic stacktrace uapkif asn1 ba-utils parson cm-pkcs12 dirent-internal)
+target_compile_definitions(uapki PRIVATE UAPKI_STATIC UAPKI_LIBRARY NOCRYPT _CRT_SECURE_NO_WARNINGS)
 
 # --- Додаємо curl ---
 set(CURL_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/curl/include")
-set(CURL_LIBRARY "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/curl/builds/windows_x86-64/libcurl.lib")
+
+# Выбираем библиотеку libcurl в зависимости от целевой архитектуры
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    # 64-bit архитектура (x64)
+    set(CURL_LIBRARY "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/curl/builds/windows_x86-64/libcurl.lib")
+else()
+    # 32-bit архитектура (x86)
+    set(CURL_LIBRARY "${CMAKE_SOURCE_DIR}/extern/uapki/library/common/curl/builds/windows_x86/libcurl.lib")
+endif()
+
+message(STATUS "[UAPKI] Using libcurl: ${CURL_LIBRARY}")
 target_link_libraries(uapki PUBLIC ${CURL_LIBRARY})
 
-# --- Створюємо uapki_bundle ---
+# --- Создаем объединенную библиотеку всех компонентов UAPKI ---
+if(TARGET uapki_full_static)
+    return()
+endif()
+
+# Создаем объединенную статическую библиотеку, которая включает все символы
+add_library(uapki_full_static STATIC 
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/byte-array-internal.c"
+    "${CMAKE_SOURCE_DIR}/extern/uapki/library/uapkic/src/stacktrace.c"
+)
+
+# Линкуем все библиотеки с WHOLE_ARCHIVE опцией, чтобы включить все символы
+if(WIN32)
+    # Для Windows используем специальный синтаксис MSVC
+    target_link_libraries(uapki_full_static PUBLIC 
+        uapki
+        uapkic
+        byte-array
+        ba-utils
+        stacktrace
+        dirent-internal
+        uapkif
+        asn1
+        parson
+        cm-pkcs12
+        ${CURL_LIBRARY}
+        wldap32 crypt32 ws2_32 winmm bcrypt
+    )
+else()
+    # Для Linux и других платформ
+    target_link_libraries(uapki_full_static PUBLIC
+        -Wl,--whole-archive
+        uapki
+        uapkic
+        byte-array
+        ba-utils
+        stacktrace
+        dirent-internal
+        uapkif
+        asn1
+        parson
+        cm-pkcs12
+        -Wl,--no-whole-archive
+        ${CURL_LIBRARY}
+    )
+endif()
+
+# --- Создаём uapki_bundle (интерфейсная библиотека) ---
 if(TARGET uapki_bundle)
     return()
 endif()
 add_library(uapki_bundle INTERFACE)
-target_link_libraries(uapki_bundle INTERFACE
-    uapki
-    uapkic
-    uapkif
-    asn1
-    ba-utils
-    parson
-    cm-pkcs12
-    dirent-internal
-    ${CURL_LIBRARY}
-)
+target_link_libraries(uapki_bundle INTERFACE uapki_full_static)
 # Системные библиотеки для Windows
 if(WIN32)
     target_link_libraries(uapki_bundle INTERFACE wldap32 crypt32 ws2_32 winmm bcrypt)
