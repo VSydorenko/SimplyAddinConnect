ba1daec0f887dab9fe3638a39d2514167ac3416f
Volodymyr Sydorenko <v.m.sydorenko@gmail.com>
2026-09-16 16:31:31 +0300

    chore: збірка 3.2.0.218 для релізу v3.2.0.218

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define VER_FILENAME SimplyAddinConnect
-#define VERSION_FULL 3.1.3.217
+#define VERSION_FULL 3.2.0.218
 #define VERSION_MAJOR     3
-#define VERSION_MINOR     1
-#define VERSION_REVISION  3
-#define VERSION_BUILD     217
+#define VERSION_MINOR     2
+#define VERSION_REVISION  0
+#define VERSION_BUILD     218
